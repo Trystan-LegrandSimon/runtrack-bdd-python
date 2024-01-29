4ec039da6280ae3be29badd01e25676149868aaa
Trysta-LegrandSimon <trystan.legrand-simon@laplateforme.io>
2024-01-29 11:44:52 +0100

    update

--- Jour01/Job05/Job05.sql
+++ Jour01/Job05/Job05.sql
@@ -4,7 +4,7 @@ USE LaPlateforme;
 
 INSERT INTO etudiant (nom, prenom, age, email) VALUES
     ('Spaghetti', 'Betty', 23, 'betty.Spaghetti@laplateforme.io'),
-    ('Steak', 'Chuck', 45, 'chuck.steak@laplateforme.io'),
+    ('Chuck', 'Steak', 45, 'chuck.steak@laplateforme.io'),
     ('Doe', 'John', 18, 'john.doe@laplateforme.io'),
     ('Barnes', 'Binkie', 16, 'binkie.barnes@laplateforme.io'),
     ('Dupuis', 'Gertrude', 20, 'gertrude.dupuis@laplateforme.io');
