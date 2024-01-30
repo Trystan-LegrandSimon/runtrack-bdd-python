0db01e8fbd2b7a433fd71643bbbd7f46ed6d5864
Trysta-LegrandSimon <trystan.legrand-simon@laplateforme.io>
2024-01-30 11:01:53 +0100

    Jour01 : UPDATE Job08

--- Jour01/Job08/Job08.sql
+++ Jour01/Job08/Job08.sql
@@ -1,4 +1,5 @@
--- Active: 1706522342473@@127.0.0.1@3306@LaPlateforme
+-- Sélectionner la base de données
+USE LaPlateforme;
 
 /* On sélection tout les étudiant qui ont moins de 18 ans*/
 SELECT * FROM etudiant WHERE age < 18;
