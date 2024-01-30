34ea870a2a08cdc96d64fd43e5a628f1db483936
Trysta-LegrandSimon <trystan.legrand-simon@laplateforme.io>
2024-01-30 11:06:19 +0100

    Jour01 : UPDATE Job12

--- Jour01/Job12/job12.sql
+++ Jour01/Job12/job12.sql
@@ -1,3 +1,6 @@
+-- Sélectionner la base de données
+USE LaPlateforme;
+
 -- Ajouter Martin Dupuis à la table etudiant
 INSERT INTO etudiant (nom, prenom, age, email) VALUES ('Dupuis', 'Martin', 18, 'martin.dupuis@laplateforme.io');
 
