3ab2a0b60168ed9dd224f8dbcf8226527622a4ec
Trysta-LegrandSimon <trystan.legrand-simon@laplateforme.io>
2024-01-30 11:07:44 +0100

    Jour01 : UPDATE Job14

--- Jour01/Job14/job14.sql
+++ Jour01/Job14/job14.sql
@@ -1,1 +1,5 @@
+-- Sélectionner la base de données
+USE LaPlateforme;
+
+/* On sélection tout les étudiant qui ont entre 18 et 25 ans dans l'ordre croissant */
 SELECT * FROM etudiant WHERE age BETWEEN 18 AND 25 ORDER BY age ASC;
