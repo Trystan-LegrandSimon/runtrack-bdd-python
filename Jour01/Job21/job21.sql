d3a0e5aea8b9db42c5a6ad662d7a67a9649aa1d9
Trysta-LegrandSimon <trystan.legrand-simon@laplateforme.io>
2024-01-30 11:13:39 +0100

    Jour01 : UPDATE Job21

--- Jour01/Job21/job21.sql
+++ Jour01/Job21/job21.sql
@@ -1,2 +1,5 @@
+-- Sélectionner la base de données
+USE LaPlateforme;
+
 -- Compter le nombre d'étudiants dont l'âge est compris entre 18 et 25 ans
 SELECT COUNT(*) AS nombre_etudiants_18_25 FROM etudiant WHERE age BETWEEN 18 AND 25;
