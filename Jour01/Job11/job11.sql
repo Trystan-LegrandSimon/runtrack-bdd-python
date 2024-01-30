58080faad9a43256528fde7ddc5bab01a8e94ecf
Trysta-LegrandSimon <trystan.legrand-simon@laplateforme.io>
2024-01-30 11:05:29 +0100

    Jour01 : UPDATE Job11

--- Jour01/Job11/job11.sql
+++ Jour01/Job11/job11.sql
@@ -1,1 +1,5 @@
+-- Sélectionner la base de données
+USE LaPlateforme;
+
+-- On sélection tout les étudiant avec le nom 'Dupuis' et le prénom 'Gertrude'
 SELECT * FROM etudiant WHERE nom = 'Dupuis' AND prenom = 'Gertrude';
